5b159767f7f230bb05b70d663deffedca93aedbd
JavierJamnik <jjamnik@consulmed.com.ar>
2023-05-23 10:45:13 -0300

    cambio integrador.js

--- js/integradorBoot.js
+++ js/integradorBoot.js
@@ -1,4 +1,4 @@
-// alert("Hola js");
+
 
 function calcularPrecioFinal() {
     // lo coloco para ver si ingresó a la función
@@ -13,17 +13,59 @@ function calcularPrecioFinal() {
     console.log("desc:" + desc);
 
     // obtento el descuento que hay que realizar
-    let descTotal= (cantEnt*1000) * desc/100;
+    let descTotal= (cantEnt*200) * desc/100;
     //10 *1000 * 10/ 100=1000
     console.log("descTotal" + descTotal);
 
     // resto el descuento total del precio total
-    let precioFin=(cantEnt*1000) - descTotal;
+    let precioFin=(cantEnt*200) - descTotal;
     console.log("precioFin:" + precioFin);
 
 
     // coloco en el html el valor de las entradas - el descuento
-    document.getElementById("precioFinal").value=precioFin;
-    document.getElementById("precioFinal2").innerHTML=precioFin;
-    document.getElementById("precioFinal2").style.display="none";
+    document.getElementById("precioFinal").value=`Total a pagar: $ ${precioFin}`;
+    //document.getElementById("precioFinal2").innerHTML=precioFin;
+    //document.getElementById("precioFinal2").style.display="none";
+
+    return precioFin
 }
+
+function limpiarInput() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("cantEntradas").value = "";
+    
+    // document.getElementsByTagName("input").value = "";
+}
+/*
+id="nombre"
+id="apellido"
+id="email"
+id="cantEntradas"
+id="porcDescuento"
+id="precioFinal"
+*/
+
+
+function resumen() {
+    let nombreGet = document.getElementById("nombre").value;
+    document.getElementById("nombreMod").innerHTML=`Nombre: ${nombreGet}`; 
+    let apellidoGet = document.getElementById("apellido").value;
+    document.getElementById("apellidoMod").innerHTML = `Apellido: ${apellidoGet}`;
+    let emailGet = document.getElementById("email").value;
+    console.log("email :" + emailGet);
+    document.getElementById("emailMod").innerHTML = `Email: ${emailGet}`;
+    let cantEnt=document.getElementById("cantEntradas").value; 
+    document.getElementById("cantidadMod").innerHTML = `Total de entradas : ${cantEnt}`;
+    // let precioFin2 = calcularPrecioFinal()
+    // console.log("precioFin2" + precioFin2)
+    document.getElementById("totalMod").innerHTML=`Total a pagar: $ ${calcularPrecioFinal()}`;
+
+} 
+/*
+cantEnt
+desc
+descTotal
+precioFin
+*/
